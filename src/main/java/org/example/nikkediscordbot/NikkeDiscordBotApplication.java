97f3c107b0a60a61b96d92cee04557f9af064226
boxinthechaos <kdmwp310@gmail.com>
2026-06-04 14:03:55 +0900

    first commit

---
+++ src/main/java/org/example/nikkediscordbot/NikkeDiscordBotApplication.java
@@ -0,0 +1,13 @@
+package org.example.nikkediscordbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikkeDiscordBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikkeDiscordBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/nikkediscordbot/NikkeDiscordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nikkediscordbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikkeDiscordBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
